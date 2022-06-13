fbda5790e7db74aa9696e7049d1f6ea437fde868
Wamaitha <wamaithaweru19@gmail.com>
2022-06-13 16:22:29 +0300

    edit Review Form

--- review/forms.py
+++ review/forms.py
@@ -1,3 +1,4 @@
+from click import style
 from django.contrib import admin
 from django import forms
 from .models import Review,RATE_CHOICES
@@ -5,7 +6,7 @@ from .models import Review,RATE_CHOICES
 
 
 class Rateform(forms.ModelForm):
-       reviews=forms.CharField(widget=forms.Textarea(attrs={'class':"textarea is-warning"}),required=False)
+       reviews=forms.CharField(widget=forms.Textarea(attrs={'class':"textarea"}),required=False)
        rate_by_design=forms.ChoiceField(choices=RATE_CHOICES, widget=forms.Select(attrs={'class':"select is-rounded"}), required=True)
        rate_by_usability=forms.ChoiceField(choices=RATE_CHOICES, widget=forms.Select(attrs={'class':"select is-rounded"}), required=True)
        rate_by_content=forms.ChoiceField(choices=RATE_CHOICES, widget=forms.Select(attrs={'class':"select is-rounded"}), required=True)
