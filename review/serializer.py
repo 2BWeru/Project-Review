1b1c18128a21c0574782b80329ed7b4d5f822b04
Wamaitha <wamaithaweru19@gmail.com>
2022-06-13 15:22:29 +0300

    create ProfileList and ProjectList

---
+++ review/serializer.py
@@ -0,0 +1,16 @@
+from cProfile import Profile
+from rest_framework import serializers
+from .models import Profile,Projects
+
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class ProjectsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = '__all__'
